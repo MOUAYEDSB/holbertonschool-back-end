962c480ab144e640635a21e2a4e9b71dc0ec5b9e
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-23 14:43:59 +0100

    gather data from an api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,31 +5,18 @@
 import requests
 from sys import argv
 
-def fetch_user_info(user_id):
-    response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
-    return response.json()
-
-def fetch_todo_list(user_id):
-    response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id))
-    return response.json()
 
 if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Usage: {} <user_id>".format(argv[0]))
-        exit(1)
-    
-    user_id = argv[1]
-    
-    try:
-        user_info = fetch_user_info(user_id)
-        todo_list = fetch_todo_list(user_id)
-        
-        completed_tasks = [task for task in todo_list if task['completed']]
-        
-        print("Employee {} is done with tasks ({}/{}):".format(user_info['name'], len(completed_tasks), len(todo_list)))
-        for task in completed_tasks:
-            print("\t{}".format(task['title']))
-            
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
-        exit(1)
+    user_page = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".
+        format(argv[1])).json()
+    todo_page = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".
+        format(argv[1])).json()
+    titles = []
+    for task in todo_page:
+        if task.get('completed') is True:
+            titles.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user_page.get('name'), len(titles), len(todo_page)))
+    print("\n".join("\t {}".format(task) for task in titles))
